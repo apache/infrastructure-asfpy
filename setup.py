66109765ebf8b140312533d061e7fae70eb04ca7
Daniel Gruno <humbedooh@apache.org>
2022-09-12 10:28:41 -0500

    bump to 0.41, add extra_requires for aioldap

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ import setuptools
 
 def main():
     setuptools.setup(name='asfpy',
-        version='0.40',
+        version='0.41',
         description='ASF Common Python Methods',
         long_description="This is a common set of functions used by the ASF Infrastructure team such as libraries for sending email, ldap management and generic process daemonization.",
         long_description_content_type = "text/plain",
@@ -39,6 +39,7 @@ def main():
         ],
         extras_requires= {
             'ldap': ['python-ldap'],
+            'aioldap': ['bonsai'],
         },
         zip_safe=False)
 
